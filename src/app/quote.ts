4cb2b655750c78957e6597890db67b29905bc145
Christine <you@example.com>
2019-04-28 20:46:55 +0300

    added published argument to the constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {
-    constructor(public name: string, public author: string, public submitted: string) {}
+    constructor(public name: string, public author: string, public submitted: string, public published: Date) {}
 }
